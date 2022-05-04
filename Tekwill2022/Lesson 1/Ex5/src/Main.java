68fbadaefd9f22d6201a7991ab60ee0d2375c61c
Sergiu <nixor91@gmail.com>
2022-05-05 00:23:33 +0300

    First commit !!!

--- Tekwill2022/Lesson 1/Ex5/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("O X X");
-        System.out.println("O X O");
-        System.out.println("X O X");
-    }
-}
