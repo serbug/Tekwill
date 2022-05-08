d2769ed349185ffb3b0111bf69f91aa28b4fd8f4
serbug <pirjoala@mail.ru>
2022-05-08 23:41:05 +0300

    Second commit

--- Lesson_5/Class_Work/Ex5.13/src/Main.java
+++ Lesson_5/Class_Work/Ex5.13/src/Main.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-/*
-   # Tekwill Academy
-   # Java Fundamentals
-   # Exercise 5.13 Maximum element divisible by four
-   # Author: Sergiu
+/**
+   Tekwill Academy
+   Java Fundamentals
+   Exercise 5.13 Maximum element divisible by four
+   @author Sergiu
 */
 public class Main {
     public static void main(String[] args) {
-        /* Condition: Given a sequence of natural numbers, not exceeding 30000. Find the maximum element
+        /** Condition: Given a sequence of natural numbers, not exceeding 30000. Find the maximum element
          divisible by 4. The program receives the number of elements in the sequence and then the elements
          themselves as input. In the sequence, there is always an element divisible by 4. The number of
          elements does not exceed 1000. The program should print a single number: the maximum element of
