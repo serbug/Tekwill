c4affb0e27297ad3a7cab1067a592edfc4481991
serbug <pirjoala@mail.ru>
2022-06-01 23:16:20 +0300

    First commit

---
+++ Lesson_11/Home_Work/Ex11.1/src/Main.java
@@ -0,0 +1,19 @@
+import java.text.MessageFormat;
+
+/**
+ * Tekwill Academy
+ * Java Fundamentals
+ * Exercise Ex11.1 Only true or false
+ *
+ * @Author Sergiu
+ */
+
+/*Condition: Implement the method toPrimitive(). It should take a value of Boolean type and
+return a boolean. If the passed value is null, the result should be false.
+*/
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Primitive.toPrimitive(null));
+    }
+}
---
+++ Lesson_11/Home_Work/Ex11.1/src/Primitive.java
@@ -0,0 +1,8 @@
+public class Primitive {
+    public static boolean toPrimitive(Boolean b) {
+        if(!b){
+            return b;
+        }
+       return true;
+    }
+}
