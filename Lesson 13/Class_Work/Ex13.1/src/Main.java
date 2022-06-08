5cfaad53f45fc309d24914cdc30030e851a64d07
Sergiu <nixor91@gmail.com>
2022-06-08 19:49:24 +0300

    First commit

---
+++ Lesson 13/Class_Work/Ex13.1/src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Tekwill Academy
+ * Java Fudamentals
+ * Exercises Ex13.1
+ *
+ * @Author Sergiu
+ */
+public class Main {
+    public static void main(String[] args) {
+        Time time = new Time(23,59,59);
+
+        Time time2 = new Time(01,01,01);
+        System.out.println(time);
+        //System.out.println(time1);
+        System.out.println(time2);
+    }
+}
