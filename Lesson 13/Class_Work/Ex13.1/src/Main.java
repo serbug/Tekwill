3e520fb480eb0a5f404d6c8b40ae943ba9a5c0e3
Sergiu <nixor91@gmail.com>
2022-06-08 22:57:44 +0300

    Second commit

--- Lesson 13/Class_Work/Ex13.1/src/Main.java
+++ Lesson 13/Class_Work/Ex13.1/src/Main.java
@@ -1,6 +1,6 @@
 /**
  * Tekwill Academy
- * Java Fudamentals
+ * Java Fundamentals
  * Exercises Ex13.1
  *
  * @Author Sergiu
@@ -8,10 +8,11 @@
 public class Main {
     public static void main(String[] args) {
         Time time = new Time(23,59,59);
-
+        Time time1 = new Time(11,07,05);
         Time time2 = new Time(01,01,01);
+
         System.out.println(time);
-        //System.out.println(time1);
+        System.out.println(time1);
         System.out.println(time2);
     }
 }
