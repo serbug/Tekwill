d8bb05a7c816f98c511b93e1dedac6fbc23ccc16
serbug <pirjoala@mail.ru>
2022-06-15 23:49:51 +0300

    First commit

--- Lesson 15/Class_Work/Ex15.3/src/ArrayUtils.java
+++ Lesson 15/Class_Work/Ex15.3/src/ArrayUtils.java
@@ -1,6 +1,23 @@
-public class ArrayUtils {
+import java.util.Objects;
+
+public class ArrayUtils<T> {
 
     private T array;
 
-    public static <T> int
+    public T getArray() {
+        return array;
+    }
+
+    public void setArray(T array) {
+        this.array = array;
+    }
+
+    public ArrayUtils(T array) {
+    }
+    public static <E> int getFirst(int[] array) {
+        if(array!=null && array.length!=0){
+            return array[0];
+        }
+        return 0;
+    }
 }
--- Lesson 15/Class_Work/Ex15.3/src/Main.java
+++ Lesson 15/Class_Work/Ex15.3/src/Main.java
@@ -1,5 +1,20 @@
+/**
+ * Tekwill Academy
+ * Java Fundamentals
+ * Exercise Ex15.3 The first element
+ *
+ * @Author Sergiu
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int[] array = new int[]{10,2,3,6};
+       int first= ArrayUtils.getFirst(array);
+
+        if (first == 0) {
+            System.out.println("null");
+        }else{
+            System.out.println(first);
+        }
+
     }
 }
