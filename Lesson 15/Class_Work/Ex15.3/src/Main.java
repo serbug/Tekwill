418cec7d57fc6389f487374c2f4b0903adba81a4
serbug <pirjoala@mail.ru>
2022-06-15 23:51:31 +0300

    First commit

--- Lesson 15/Class_Work/Ex15.3/src/Main.java
+++ Lesson 15/Class_Work/Ex15.3/src/Main.java
@@ -8,6 +8,7 @@
 public class Main {
     public static void main(String[] args) {
        int[] array = new int[]{10,2,3,6};
+       //int[] array=null;
        int first= ArrayUtils.getFirst(array);
 
         if (first == 0) {
